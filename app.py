ee7e2add6eba0660c538d92d81a7992cc05e01ce
jpmscit20 <jpmscit20@gmail.com>
2023-12-13 19:54:26 +0530

    app modified

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-1   from flask import Flask
-  4 
-  5 app = Flask(__name__)
- 11 
- 12 @app.route('/', methods=['GET'])
- 13 def one():
- 16     return "<h1>flask_app</h1>"
- 17 
- 18 if __name__  == '__main__':
- 19     app.run(host="0.0.0.0", debug=True)
+from flask import Flask
+
+app = Flask(__name__)
+ 
+@app.route('/', methods=['GET'])
+def one():
+    return "<h1>flask_app</h1>"
+ 
+if __name__  == '__main__':
+     app.run(host="0.0.0.0", debug=True)
 
